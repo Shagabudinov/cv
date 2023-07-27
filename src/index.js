f3e345fc7bd33059728618f83cc7773050b78392
Shagabudinov <126676680+Shagabudinov@users.noreply.github.com>
2023-07-27 22:03:39 +0300

    neww-commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './styles/ButtonStyles.sass';
 import App from './App.js';
 
 //dwdsd
+//gfgsfgas
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
